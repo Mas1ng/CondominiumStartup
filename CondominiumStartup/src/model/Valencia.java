f06a6840f3052f15fa5d8aeeb597a3f3950b1a87
1200766 <128920014+1200766@users.noreply.github.com>
2023-03-25 22:48:14 +0000

    Update Valencia.java

--- CondominiumStartup/src/model/Valencia.java
+++ CondominiumStartup/src/model/Valencia.java
@@ -24,37 +24,7 @@ public class Valencia implements Serializable{
     public void setCusto(float custo) {
         this.custo = custo;
     }
-    public Valencias(String valencia) {
-        checkValencias(valencia);
-    }
-    public String getValencias() {
-        return valencias;
-    }
 
-    public void setContact(int contact) {
-        checkContact(contact);
-    }
-
-    private void checkValencias(String valencias) throws ValenciasInvalidException {//criar exceção
-        if (eValidValencias(valencias) == true) {
-            this.valencias = valencias;
-        } else {
-            throw new DataInvalidaException("Valência inválida.");
-        }
-    }
-
-    private boolean eValidContact(int contact){
-        Pattern p = Pattern.compile("^\\d{9}$");
-        Matcher m = p.matcher(Integer.toString(contact));
-        return (m.matches());
-    }
-
-    @Override
-    public String toString() {
-        return "Contact{" +
-                "Contato=" + contact +
-                '}';
-    }
 }
 
 
